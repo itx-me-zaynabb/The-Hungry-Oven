8410a818067ce8921eaabe9cdc03881f70df1e86
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-05-04 19:35:54 +0500

    add theme

---
+++ tailwind.confg.js
@@ -0,0 +1,15 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7a0c0c", // maroon
+        dark: "#0a0a0a",
+        cheese: "#fbbf24",
+      },
+      fontFamily: {
+        display: ["Bebas Neue", "sans-serif"],
+        body: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
