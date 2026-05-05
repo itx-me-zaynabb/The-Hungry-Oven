8c0176fb86deaac9a82e1d83244b3732f4831539
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-05-05 16:55:43 +0500

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <CustomCursor />
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
