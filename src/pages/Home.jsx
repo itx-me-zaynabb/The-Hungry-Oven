0d1402dcf144d57a972391fe751ac145f902c60c
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-05-05 16:57:00 +0500

    change css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="pt-24 h-screen flex items-center justify-center">
       <h1 className="text-6xl font-display text-cheese">The Hungry Oven 🍕</h1>
     </div>
   );
