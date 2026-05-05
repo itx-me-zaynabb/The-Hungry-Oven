e45b7f519c4d61b34b388412efa13b55bac21abb
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-05-05 16:54:24 +0500

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Deals from "./pages/Deals";
 import Contact from "./pages/Contact";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <CustomCursor />
-      <Home />
-      <Navbar />
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/deals" element={<Deals />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
