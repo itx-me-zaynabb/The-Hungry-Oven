b4c61d9810fec52144b98398145731ca50b0e710
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-05-07 18:09:29 +0500

    import Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import CustomCursor from "./components/CustomCursor";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -5,17 +7,26 @@ import Menu from "./pages/Menu";
 import Deals from "./pages/Deals";
 import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
   return (
     <>
       <CustomCursor />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
+        <Route path="/menu" element={<Menu cart={cart} setCart={setCart} />} />
+
         <Route path="/deals" element={<Deals />} />
         <Route path="/contact" element={<Contact />} />
+
+        <Route
+          path="/cart"
+          element={<Cart cartItems={cart} setCart={setCart} />}
+        />
       </Routes>
     </>
   );
