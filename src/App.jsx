c5fe0923c1fad0f1fd95d09b31bcbeed6397e244
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-05-11 16:09:36 +0500

    remove cart

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,24 @@ import Menu from "./pages/Menu";
 import Deals from "./pages/Deals";
 import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
-import Cart from "./pages/Cart";
 import AdminDashboard from "./pages/AdminDashboard";
 import CreateBill from "./pages/CreateBill";
 import Login from "./pages/Login";
 
 function App() {
-  const [cart, setCart] = useState([]);
-
   return (
     <>
       <CustomCursor />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu cart={cart} setCart={setCart} />} />
+        <Route path="/menu" element={<Menu />} />
 
         <Route path="/deals" element={<Deals />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/cart"
-          element={<Cart cartItems={cart} setCart={setCart} />}
-        />
+
         <Route path="/create-bill" element={<CreateBill />} />
       </Routes>
     </>
