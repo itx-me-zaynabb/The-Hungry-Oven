389e783ae3217b6d092f327d23effe01a57d8b2b
Zainab Fatima <zainabfattimaa@gmail.com>
2026-05-21 18:49:44 +0500

    fixed public home and admin auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import CustomCursor from "./components/CustomCursor";
 import Navbar from "./components/Navbar";
+
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
@@ -11,50 +12,31 @@ import AdminDashboard from "./pages/AdminDashboard";
 import CreateBill from "./pages/CreateBill";
 
 function App() {
-  const token = localStorage.getItem("token");
   const role = localStorage.getItem("role");
 
   return (
     <>
-      {/* NAVBAR ONLY AFTER LOGIN */}
-      {token && <Navbar />}
+      {/* NAVBAR ALWAYS */}
+      <Navbar />
       <CustomCursor />
+
       <Routes>
-        {/* LOGIN */}
-        <Route path="/" element={token ? <Navigate to="/home" /> : <Login />} />
+        {/* PUBLIC PAGES */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/deals" element={<Deals />} />
+        <Route path="/contact" element={<Contact />} />
 
-        {/* HOME */}
-        <Route path="/home" element={token ? <Home /> : <Navigate to="/" />} />
-
-        {/* MENU */}
-        <Route path="/menu" element={token ? <Menu /> : <Navigate to="/" />} />
-        <Route
-          path="/deals"
-          element={token ? <Deals /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/contact"
-          element={token ? <Contact /> : <Navigate to="/" />}
-        />
-
-        {/* ADMIN */}
+        {/* ADMIN ONLY */}
         <Route
           path="/admin"
-          element={
-            token && role === "admin" ? (
-              <AdminDashboard />
-            ) : (
-              <Navigate to="/home" />
-            )
-          }
+          element={role === "admin" ? <AdminDashboard /> : <Navigate to="/" />}
         />
 
-        {/* CREATE BILL */}
         <Route
           path="/create-bill"
-          element={
-            token && role === "admin" ? <CreateBill /> : <Navigate to="/home" />
-          }
+          element={role === "admin" ? <CreateBill /> : <Navigate to="/" />}
         />
       </Routes>
     </>
